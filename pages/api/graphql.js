2c1ee9ede1c3c3a0cf0cde0ab8cfea110e00f461
Victor <victor.ribero3@gmail.com>
2021-07-23 16:38:41 -0600

    added cors config

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -2,7 +2,14 @@ import { ApolloServer } from "apollo-server-micro";
 import { typeDefs } from "./schemas";
 import { resolvers } from "./resolvers";
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
 
 export const config = {
   api: {
