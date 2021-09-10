975153df02c9ede602cc76b1ab8987ebbf37f865
victorcrystallize <victor@crystallize.com>
2021-09-09 20:43:04 -0600

    add mongoDB connection to graphql context

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -2,19 +2,35 @@ import { ApolloServer } from "apollo-server-micro";
 import { typeDefs } from "./schemas";
 import { resolvers } from "./resolvers";
 import withCors from "../../lib/cors";
+import { MongoClient } from "mongodb";
 
-const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
-  cors: {
-    origin: true,
-  },
-});
+export default async function handler(req, res) {
+  // Connection to Database
+  const dbUri = process.env.DB_URI;
+  const clientDB = new MongoClient(
+    dbUri
+      .replace("<db_user>", process.env.DB_USER)
+      .replace("<db_password>", process.env.DB_PASSWORD)
+  );
+
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    cors: { origin: true },
+    context: { clientDB },
+  });
+
+  const apiRequestHandler = apolloServer.createHandler({
+    path: "/api/graphql",
+  });
+
+  // By default, NextJS doesn't add any headers, so we have to add
+  // the headers that allows to use CORS manually.
+  return withCors(apiRequestHandler)(req, res);
+}
 
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-
-export default withCors(apolloServer.createHandler({ path: "/api/graphql" }));
